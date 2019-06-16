be357e85a3ff2a65aec3d9031165efb91e615ca7
Daria Magiera <44238676+dariamagiera@users.noreply.github.com>
2019-06-16 17:19:10 +0200

    Update Day.cpp

--- Day.cpp
+++ Day.cpp
@@ -1,14 +1,8 @@
-//
-// Created by Daria Magiera on 2019-06-15.
-//
 #include "Day.h"
 
-
 Day::Day() {
-
     events.reserve(10);
     availability = true;
-
 }
 
 void Day::setAvailability(bool input) {
@@ -18,5 +12,3 @@ void Day::setAvailability(bool input) {
 bool Day::isAvailable() {
     return availability;
 }
-
-
